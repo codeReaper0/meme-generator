1f703e97caebfbaaf547bf88eb8128ef36d9f353
Tella Boluwatife <boluwatifetella@gmail.com>
2022-12-16 15:28:06 +0100

    meme generator button done

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,21 @@
 function Content() {
   return (
     <main className="max-w-6xl mx-auto p-5">
-      <div className="flex gap-2">
-        <img src="/images/troll.png" alt="memes by tella" className="" />
-        <h className="text-white font-bold text-xl">Meme Generator</h>
-      </div>
+      <form className="grid grid-cols-2 grid-rows-[40px_40px] gap-4">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="rounded-[5px] border border-[#d5d4d8] indent-[5px]"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="rounded-[5px] border border-[#d5d4d8] indent-[5px]"
+        />
+        <button className="col-span-2  gradient-bg text-white rounded-[5px] border-none cursor-pointer">
+          Get a new meme image 🖼
+        </button>
+      </form>
     </main>
   );
 }
